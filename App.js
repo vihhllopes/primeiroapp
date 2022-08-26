94b9d8230c7b122beb3aecc81826114ad8c00fca
victoria <viictoria931@gmail.com>
2022-08-26 14:16:01 -0300

    app

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, Image, Alert} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+       <Image source={require('../primerioappp/assets/mamaativa.png')} />
+       <Button style={styles.button}
+  
+                // Some properties given to Button
+                title="Entrar"
+                onPress={() => Alert.alert(
+                    'Aplicativo em Construção!')} />
     </View>
   );
 }
@@ -13,8 +19,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9098E0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+   padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+  }
 });
+
